627eeaa60b51eedc597b43d1fdbbeeabbffb5a1c
laurawilsone <laura.wils204@gmail.com>
2018-06-08 15:09:53 -0500

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ var connection;
      connection = mysql.createConnection(process.env.JAWSDB_URL);
  } else {
   connection = mysql.createConnection({
-  port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
